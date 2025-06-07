e8defaee602f6200488e82718900610e8ca5a5dc
Thais Luca <thais.luca95@gmail.com>
2025-06-06 22:46:38 -0300

    Add model that performs no transfer

--- easy/model_sider.py
+++ easy/model_sider.py
@@ -136,6 +136,49 @@ class GNN_graphpred_side(GNN_graphpred):
         return model
 
 
+class GNN_graphpred_no_transfer(GNN_graphpred):
+    def __init__(self, num_layer, emb_dim, num_tasks, num_side_layer=3, down_dim=16, JK = "last", drop_ratio = 0, graph_pooling = "mean"):
+        super().__init__(num_layer, emb_dim, num_tasks, JK, drop_ratio, graph_pooling)
+
+    
+    def forward(self, *argv):
+        if len(argv) == 3:
+            x, edge_index, edge_attr = argv[0], argv[1], argv[2]
+        elif len(argv) == 1:
+            data = argv[0]
+            x, edge_index, edge_attr = data.x, data.edge_index, data.edge_attr
+        else:
+            raise ValueError("unmatched number of arguments.")
+
+        x = self.x_embedding1(x[:,0]) + self.x_embedding2(x[:,1])
+
+        h_list = [x]
+        for layer in range(self.num_layer):
+            h = self.gnns[layer](h_list[layer], edge_index, edge_attr)
+            h = self.batch_norms[layer](h)
+            #h = F.dropout(F.relu(h), self.drop_ratio, training = self.training)
+            if layer == self.num_layer - 1:
+                #remove relu for the last layer
+                h = F.dropout(h, self.drop_ratio, training = self.training)
+            else:
+                h = F.dropout(F.relu(h), self.drop_ratio, training = self.training)
+            h_list.append(h)
+
+        ### Different implementations of Jk-concat
+        if self.JK == "concat":
+            node_representation = torch.cat(h_list, dim = 1)
+        elif self.JK == "last":
+            node_representation = h_list[-1]
+        elif self.JK == "max":
+            h_list = [h.unsqueeze_(0) for h in h_list]
+            node_representation = torch.max(torch.cat(h_list, dim = 0), dim = 0)[0]
+        elif self.JK == "sum":
+            h_list = [h.unsqueeze_(0) for h in h_list]
+            node_representation = torch.sum(torch.cat(h_list, dim = 0), dim = 0)[0]
+
+        return node_representation
+
+
 if __name__ == "__main__":
             # Training settings
     parser = argparse.ArgumentParser(description='PyTorch implementation of transfer of graph neural networks')
