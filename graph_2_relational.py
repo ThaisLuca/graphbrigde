6030f8ddd6a7dd1b66c46e59d84c5b56a22e89d5
Thais Luca <thais.luca95@gmail.com>
2026-01-10 14:39:45 -0300

    Add files via upload

--- graph_2_relational.py
+++ graph_2_relational.py
@@ -1,4 +1,5 @@
 
+import json
 import torch
 import networkx as nx
 import numpy as np
@@ -38,6 +39,27 @@ def convert(path, dataset, check_sanity=False):
 
     data_name = dataset
     target_name = datasets_to_go_relational[dataset]["target"]
+
+    data_to_json = [
+        [
+            {
+                target_name: [],
+                "atomicNumber": [],
+                "chiralityNumber": [],
+                "bond": [],
+                "stereo": []
+            }
+        ],
+        [
+            {
+                target_name: [],
+                "atomicNumber": [],
+                "chiralityNumber": [],
+                "bond": [],
+                "stereo": []
+            }
+        ]
+    ]
     
     toxicity = False
     if dataset in ["clintox_toxicity", "clintox_approve"]:
@@ -48,65 +70,86 @@ def convert(path, dataset, check_sanity=False):
     dataset = MoleculeDataset("dataset/" + dataset, dataset=dataset)
     print(f"{dataset} has {len(dataset)} instances")
     
-    node_type_mapping, edge_type_mapping = {}, {}
     for i, data in enumerate(dataset):
 
         molecule_name = data_name + str(i+1)
 
-        # Now identify if it is a positive example!
+        # Now identify if it is a positive/negative example!
         target = data.y.tolist()[0]
 
         if target == -1:
             neg.append(
                 f"{target_name}({molecule_name})."
             )
+
+            data_to_json[1][0][target_name].append([molecule_name])
         else:
             pos.append(
                 f"{target_name}({molecule_name})."
             )
 
-        # TODO: separar apenas um fold. O que tiver mais samples
+            data_to_json[0][0][target_name].append([molecule_name])
 
         for item in range(0, data.x.shape[0]):
             atomic_number, chirality_number = data.x[item].tolist()
             
-            if atomic_number not in node_type_mapping:
-                node_type_mapping[atomic_number] = f"node{atomic_number}"
-            node_type = node_type_mapping[atomic_number]
+            node_type = f"node{item}"
 
             # All datasets have two features
             # Every feature becomes a fact
             #   [[5, 0], -> feature1(molecule1, node1, 5), feature2(molecule1, node1, 0)
             #    [6,0]]  -> feature1(molecule1, node2, 6), feature2(molecule1, node2, 0)
-            facts.append(
-                f"atomicNumber({molecule_name},{node_type},{atomic_number})."
-            )
+            if target != -1:
+            
+                facts.append(
+                    f"atomicNumber({molecule_name},{node_type},{atomic_number})."
+                )
 
-            facts.append(
-                f"chiralityNumber({molecule_name},{node_type},{chirality_number})."
-            )
+                facts.append(
+                    f"chiralityNumber({molecule_name},{node_type},{chirality_number})."
+                )
 
+            if target == -1:
+                data_to_json[1][0]["atomicNumber"].append([molecule_name,node_type,atomic_number])
+                data_to_json[1][0]["chiralityNumber"].append([molecule_name,node_type,chirality_number])
+            else:
+                data_to_json[0][0]["atomicNumber"].append([molecule_name,node_type,atomic_number])
+                data_to_json[0][0]["chiralityNumber"].append([molecule_name,node_type,chirality_number])
+        
+        for idx in range(data.num_edges):
 
-        for idx, (src, dst) in enumerate(data.edge_index.t().tolist()):
+            src = data.edge_index[0, idx]
+            dst = data.edge_index[1, idx]
 
-            src_atomic_number = data.x[src].tolist()[0]
-            dst_atomic_number = data.x[dst].tolist()[0]
+            #src_atomic_number, src_chirality_number = data.x[src].tolist()
+            #dst_atomic_number, dst_chirality_number = data.x[dst].tolist()
 
-            src_node_type, dst_node_type = node_type_mapping[src_atomic_number], node_type_mapping[dst_atomic_number]
+            src_node_type, dst_node_type = f"node{src}", f"node{dst}"
 
             # Get edge attribute
             bondtype, stereo = data.edge_attr[idx].tolist()
 
             # Every edge becomes a fact too
             #   [[5, 0]] -> bond(molecule1, node1, node2, 5), stereo(molecule1, node1, node2, 0)
-            facts.append(
-                f"bond({molecule_name},{src_node_type},{dst_node_type},{bondtype})."
-            )
 
-            facts.append(
-                f"stereo({molecule_name},{src_node_type},{dst_node_type},{stereo})."
-            )
+            if target != -1:
+                facts.append(
+                    f"bond({molecule_name},{src_node_type},{dst_node_type},{bondtype})."
+                )
 
+                facts.append(
+                    f"stereo({molecule_name},{src_node_type},{dst_node_type},{stereo})."
+                )
+
+            if target == -1:
+                data_to_json[1][0]["bond"].append([molecule_name,src_node_type,dst_node_type,bondtype])
+                data_to_json[1][0]["stereo"].append([molecule_name,src_node_type,dst_node_type,stereo])
+            else:
+                data_to_json[0][0]["bond"].append([molecule_name,src_node_type,dst_node_type,bondtype])
+                data_to_json[0][0]["stereo"].append([molecule_name,src_node_type,dst_node_type,stereo])
+
+    json.dump(data_to_json, open(f"{data_name}.json", "w"))
+    
     facts,pos,neg = list(set(facts)), list(set(pos)), list(set(neg))
     facts.sort()
     return facts,pos,neg
